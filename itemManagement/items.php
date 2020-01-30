6a75d7b8ded7f9948e435209374bad997fef729a
Colton Williams <coltontfw@gmail.com>
2020-01-30 08:37:10 -0800

    test

--- itemManagement/items.php
+++ itemManagement/items.php
@@ -107,7 +107,7 @@
         public function getAllItems(){
             $stmt = $this->conn->query("SELECT name FROM items");
             $allItemsArray = [];
-            while ($row = $stmt -> fetch_all(MYSQLI_ASSOC)){
+            foreach ($stmt->get_result() as $row){
                 $allItemsArray[] = $row['name'];
             }
             return json_encode($allItemsArray);
