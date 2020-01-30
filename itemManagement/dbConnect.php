fe96093aa648555759d3d69203390479b4b8e091
Colton Williams <coltontfw@gmail.com>
2020-01-29 20:53:14 -0800

    trying to connect to clearDB

--- itemManagement/dbConnect.php
+++ itemManagement/dbConnect.php
@@ -2,12 +2,6 @@
 
     class itemsDatabase {
 
-        private $servername;
-        private $username;
-        private $password;
-        private $dbName;
-        private $charset;
-
         // Please create a database with the following items and measurements in order to use the app:
 
         //     Fiddle: 1 kg, 60cm x 20cm x 10cm
@@ -20,21 +14,13 @@
 
             $url = parse_url(getenv("mysql://b4dd3dae1de623:9dd61eb4@us-cdbr-iron-east-04.cleardb.net/heroku_1d2208bffa1b10a?reconnect=true"));
 
-            $this->servername = $url["us-cdbr-iron-east-04.cleardb.net"];;
-            $this->username = $url["b4dd3dae1de623"];
-            $this->password = $url["9dd61eb4"];
-            $this->dbname = substr($url["heroku_1d2208bffa1b10a"], 1);
-            $this->charset = "utf8mb4";
+            $server = $url["us-cdbr-iron-east-04.cleardb.net"];
+            $username = $url["b4dd3dae1de623"];
+            $password = $url["9dd61eb4"];
+            $db = substr($url["heroku_1d2208bffa1b10a"], 1);
 
-            try {
-
-            $pdo = new PDO("pgsql: host=".$this->servername"; dbname=".$this->dbname, $this->username, $this->password);
-            $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-            return $pdo;
-
-            } catch (PDOException $e) {
-                echo "Connection Failed: ".$e->getMessage();
-            }
+            $conn = new mysqli($server, $username, $password, $db);
+            return $conn;
         }
 
     }
